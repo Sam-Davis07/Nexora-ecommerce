d91e55da271bc767628f9a6b0f1caa149fdb0c39
Sam-Davis07 <sam.r.davis07@gmail.com>
2026-08-11 00:38:44 +0530

    Prepare Nexora for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/cart", cartRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
